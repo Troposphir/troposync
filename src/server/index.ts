7bd63fbddc75bc7323ea5d348c7280549b67a039
kroltan <kroltan1@gmail.com>
2017-02-27 22:30:29 -0300

    server: added request logging

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,10 @@
 import * as express from "express";
+import * as morgan from "morgan";
 import {UpdatesRouter} from "./updates";
 
 const app = express();
+
+app.use(morgan("short"));
 
 app.use("/update", UpdatesRouter);
 
