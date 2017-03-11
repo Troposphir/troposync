288461720104723db8e19af66186dd48173bb573
kroltan <kroltan1@gmail.com>
2017-03-11 16:20:49 -0300

    enable gzipping responses for better performance

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,12 +1,13 @@
 import * as express from "express";
 import * as morgan from "morgan";
+import * as compression from "compression";
 import {UpdatesRouter} from "./updates";
 import {NewsRouter} from "./news";
 
 const app = express();
 
 app.use(morgan("short"));
-
+app.use(compression());
 app.use("/update", UpdatesRouter);
 app.use("/news", NewsRouter);
 
